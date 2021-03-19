72ba00ac17ee667320923eccaf0bf78ec6c38569
Jimmy Escalante <escalasa@oregonstate.edu>
2021-03-18 23:38:26 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Santiago Escalante  on 3/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
