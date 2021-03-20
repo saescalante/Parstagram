b9d4f8b4b556257128199241b342a0c2f9d9f7a7
Jimmy Escalante <escalasa@oregonstate.edu>
2021-03-20 10:44:03 -0500

    Server setup. Signup and login via parse.

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "3HIYbOkbY62V2YbVQsTPIJ9YmDBZunyizePoC9Qb"
+                $0.clientKey = "a86d2tcKAqQ893b6hjK7Vj3Ee1tykLNlP5W2pxCb"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
