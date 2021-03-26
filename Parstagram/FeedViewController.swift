89fc709f0926802831eeaf04f05b82fd972dd124
Jimmy Escalante <escalasa@oregonstate.edu>
2021-03-26 00:12:15 -0500

    Persistent log-in and logout added.

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -53,7 +53,15 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
-
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let delegate = self.view.window?.windowScene?.delegate as! SceneDelegate
+        delegate.window?.rootViewController = loginViewController
+    }
     
 
 }
